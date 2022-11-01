b02178f739009b24354cfb9cd6e8f3ad6d35bf50
Joao Winkelmann <hmaster.studio@gmail.com>
2022-11-01 11:22:12 -0300

    api.mjs formatado

--- app/api.mjs
+++ app/api.mjs
@@ -1,13 +1,22 @@
-import express from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
-app.get("/api/", (req, res) => {
-    // abra no navegador o link http://localhost:3000/api
-    const request = JSON.stringify({
-        error: false,
-        result: "Olá mundo isso é um teste",
-    });
-    res.send(request);
-});
+app.get('/api/', (req, res) => {
+        // abra no navegador o link http://localhost:3000/api
+        const request = JSON.stringify({
+                error: false,
+                result: 'Olá mundo isso é um teste',
+        })
+        res.send(request)
+})
 
-app.listen(3000);
+app.get('/api/login', (req, res) => {
+        // abra no navegador o link http://localhost:3000/api
+        const request = JSON.stringify({
+                error: false,
+                result: 'Olá login',
+        })
+        res.send(request)
+})
+
+app.listen(3000)
