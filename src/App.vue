7a769d247e675c77aba291fd33c19389f6c3b758
iury <iury2002silva@hotmail.com>
2022-11-01 19:01:36 -0300

    home-page-test

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,56 @@
 <template>
-    <header>
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
+        <div class="uk-container uk-container-expand uk-background-primary">
+                <nav class="uk-navbar ">
+                        <div class="uk-navbar-left">
+                            <RouterLink to="" uk-icon="joomla" class="uk-text-muted"></RouterLink>
+                        </div>
+                        <div class="uk-navbar-right">
+                                <ul class="uk-navbar-nav uk-visible@m">
+                                        <li><RouterLink to="">Home</RouterLink></li>
+                                        <li><RouterLink to="">Login</RouterLink></li>
+                                        <li><RouterLink to="" uk-icon="cart"></RouterLink></li>
+                                        <li><RouterLink to="" uk-icon="user"></RouterLink></li>
+                                </ul>
+                        </div>
+                </nav>
         </div>
-    </header>
+        <header>
+                <div class="wrapper">
+                        <HelloWorld msg="You did it!" />
 
-    <div>
-        <button
-            uk-toggle="target: #my-id"
-            type="button"
-            class="uk-button uk-button-primary"
-        >
-            Abrir Modal
-        </button>
-        <!-- This is the modal -->
-        <div id="my-id" uk-modal>
-            <div class="uk-modal-dialog uk-modal-body">
-                <h2 class="uk-modal-title">Isso é um Modal</h2>
-                <div class="uk-padding-large">
-                    <RouterView />
+                        <nav>
+                                <RouterLink to="/">Home</RouterLink>
+                                <RouterLink to="/about">About</RouterLink>
+                                
+                        </nav>
                 </div>
-            </div>
-        </div>
-    </div>
+        </header>
 
+        <div>
+                <button
+                        uk-toggle="target: #my-id"
+                        type="button"
+                        class="uk-button uk-button-primary"
+                >
+                        Abrir Modal
+                </button>
+                <!-- This is the modal -->
+                <div id="my-id" uk-modal>
+                        <div class="uk-modal-dialog uk-modal-body">
+                                <h2 class="uk-modal-title">Isso é um Modal</h2>
+                                <div class="uk-padding-large">
+                                        <RouterView />
+                                </div>
+                        </div>
+                </div>
+        </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
-}
 </style>
