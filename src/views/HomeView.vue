e71e36d1213d9a68e38faf2b7b613f9d44b4ba89
iury <iury2002silva@hotmail.com>
2022-11-02 12:07:29 -0300

    background-test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,10 @@
 </script>
 
 <template>
+    <div class="uk-background-image@xl uk-background-cover uk-container-xlarge ">
+
     <div class="home">
        <h1> This is an home page</h1>
     </div>    
-
+    </div>
 </template>
