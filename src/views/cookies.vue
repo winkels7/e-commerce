198b7b90d6c4febfad1b398b3f15bd52b5667a45
iury <iury2002silva@hotmail.com>
2022-11-03 15:33:07 -0300

    politicas-update

--- src/views/cookies.vue
+++ src/views/cookies.vue
@@ -1,16 +1,90 @@
 <template>
-  <div class="uk-child-width-1-2@s" uk-grid>
-    <div>
-        <div uk-grid>
-            <div class="uk-width-auto@m">
-                <ul class="uk-list">
-                    <li><RouterLink to="/privacidade" class="uk-button uk-button-text">Politica de Privacidade</RouterLink></li>
+  <div class="uk-container uk-width-1-1 uk-flex uk-margin-medium-top">
+        <div class="uk-margin-medium-right">
+                <ul class="uk-list uk-width-medium uk-background-primary uk-padding-small uk-border-rounded">
+                    <li><RouterLink to="/privacidade" class="uk-button uk-button-text">Política de Privacidade</RouterLink></li>
                     <li><RouterLink to="/cookies" class="uk-button uk-button-text">Política de cookies</RouterLink></li>
                 </ul>
-            </div>
-            <p>bsoakdoaskdosakdosakdosakdok</p>
+      </div>
+      <div>
+            <article class="uk-article">
+
+    <h1 class="uk-article-title"><a class="uk-link-reset" href="">Política de Cookies Parque das Palmeiras</a></h1>
+
+    <p class="uk-article-meta">Escrito por Parque das Palmeiras em 03 de Novembro 2022.</p>
+
+    <p class="uk-text-lead">O que são cookies?</p>
+
+    <p>Como é prática comum em quase todos os sites profissionais, este site usa cookies, que são pequenos arquivos baixados no seu computador, para melhorar sua experiência. Esta página descreve quais informações eles coletam, como as usamos e por que às vezes precisamos armazenar esses cookies. Também compartilharemos como você pode impedir que esses cookies sejam armazenados, no entanto, isso pode fazer o downgrade ou 'quebrar' certos elementos da funcionalidade do site.</p>
+
+    <p class="uk-text-lead">Como usamos os cookies?</p>
+
+    <p>Utilizamos cookies por vários motivos, detalhados abaixo. Infelizmente, na maioria dos casos, não existem opções padrão do setor para desativar os cookies sem desativar completamente a funcionalidade e os recursos que eles adicionam a este site. É recomendável que você deixe todos os cookies se não tiver certeza se precisa ou não deles, caso sejam usados ​​para fornecer um serviço que você usa.</p>
+
+    <p class="uk-text-lead">Desativar cookies</p>
+
+    <p>Você pode impedir a configuração de cookies ajustando as configurações do seu navegador (consulte a Ajuda do navegador para saber como fazer isso). Esteja ciente de que a desativação de cookies afetará a funcionalidade deste e de muitos outros sites que você visita. A desativação de cookies geralmente resultará na desativação de determinadas funcionalidades e recursos deste site. Portanto, é recomendável que você não desative os cookies.</p>
+
+    <p class="uk-text-lead">Cookies que definimos</p>
+    <ul>
+      <li>Cookies relacionados à conta</li>
+      <p>Se você criar uma conta connosco, usaremos cookies para o gerenciamento do processo de inscrição e administração geral. Esses cookies geralmente serão excluídos quando você sair do sistema, porém, em alguns casos, eles poderão permanecer posteriormente para lembrar as preferências do seu site ao sair.</p>
+   
+      <li>Cookies relacionados ao login</li>
+      <p>Utilizamos cookies quando você está logado, para que possamos lembrar dessa ação. Isso evita que você precise fazer login sempre que visitar uma nova página. Esses cookies são normalmente removidos ou limpos quando você efetua logout para garantir que você possa acessar apenas a recursos e áreas restritas ao efetuar login.</p>
+
+      <li>Cookies relacionados a boletins por e-mail</li>
+      <p>Este site oferece serviços de assinatura de boletim informativo ou e-mail e os cookies podem ser usados ​​para lembrar se você já está registrado e se deve mostrar determinadas notificações válidas apenas para usuários inscritos / não inscritos.</p>
+
+      <li>Pedidos processando cookies relacionados</li>
+      <p>Este site oferece facilidades de comércio eletrônico ou pagamento e alguns cookies são essenciais para garantir que seu pedido seja lembrado entre as páginas, para que possamos processá-lo adequadamente.</p>
+
+      <li>Cookies relacionados a pesquisas</li>
+      <p>Periodicamente, oferecemos pesquisas e questionários para fornecer informações interessantes, ferramentas úteis ou para entender nossa base de usuários com mais precisão. Essas pesquisas podem usar cookies para lembrar quem já participou numa pesquisa ou para fornecer resultados precisos após a alteração das páginas.</p>
+
+      <li>Cookies relacionados a formulários</li>
+      <p>Quando você envia dados por meio de um formulário como os encontrados nas páginas de contacto ou nos formulários de comentários, os cookies podem ser configurados para lembrar os detalhes do usuário para correspondência futura.</p>
+
+      <li>Cookies de preferências do site</li>
+      <p>Para proporcionar uma ótima experiência neste site, fornecemos a funcionalidade para definir suas preferências de como esse site é executado quando você o usa. Para lembrar suas preferências, precisamos definir cookies para que essas informações possam ser chamadas sempre que você interagir com uma página for afetada por suas preferências.</p>
+    </ul>
+
+    <p class="uk-text-lead">Cookies de Terceiros</p>
+    <p>Em alguns casos especiais, também usamos cookies fornecidos por terceiros confiáveis. A seção a seguir detalha quais cookies de terceiros você pode encontrar através deste site.</p>
+   
+    <ul><li>Este site usa o Google Analytics, que é uma das soluções de análise mais difundidas e confiáveis ​​da Web, para nos ajudar a entender como você usa o site e como podemos melhorar sua experiência. Esses cookies podem rastrear itens como quanto tempo você gasta no site e as páginas visitadas, para que possamos continuar produzindo conteúdo atraente.</li></ul>
+   
+    <p>Para mais informações sobre cookies do Google Analytics, consulte a página oficial do Google Analytics.</p>
+    <ul>
+      <li>As análises de terceiros são usadas para rastrear e medir o uso deste site, para que possamos continuar produzindo conteúdo atrativo. Esses cookies podem rastrear itens como o tempo que você passa no site ou as páginas visitadas, o que nos ajuda a entender como podemos melhorar o site para você.</li>
+      <li>Periodicamente, testamos novos recursos e fazemos alterações subtis na maneira como o site se apresenta. Quando ainda estamos testando novos recursos, esses cookies podem ser usados ​​para garantir que você receba uma experiência consistente enquanto estiver no site, enquanto entendemos quais otimizações os nossos usuários mais apreciam.</li>
+      <li>À medida que vendemos produtos, é importante entendermos as estatísticas sobre quantos visitantes de nosso site realmente compram e, portanto, esse é o tipo de dados que esses cookies rastrearão. Isso é importante para você, pois significa que podemos fazer previsões de negócios com precisão que nos permitem analizar nossos custos de publicidade e produtos para garantir o melhor preço possível.</li>
+    </ul>
+
+    <p class="uk-text-lead">Compromisso do Usuário</p>
+    <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Parque das Palmeiras oferece no site e com caráter enunciativo, mas não limitativo:</p>
+    <ul>
+      <li>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</li>
+      <li>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, generador de avisos de privacidad ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li>
+      <li>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Parque das Palmeiras, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</li>
+    </ul>
+
+    <p class="uk-text-lead">Mais informações</p>
+    <p>Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.</p>
+
+    <p>Esta política é efetiva a partir de <strong>3 novembro 2022 11:16</strong></p>
+
+    <div class="uk-grid-small uk-child-width-auto" uk-grid>
+        <div>
+            <a class="uk-button uk-button-text" href="#">Read more</a>
+        </div>
+        <div>
+            <a class="uk-button uk-button-text" href="#">5 Comments</a>
         </div>
     </div>
+
+</article>
+</div>          
     
 </div>
 
--- src/views/privacidade.vue
+++ src/views/privacidade.vue
@@ -1,16 +1,45 @@
 <template>
-  <div class="uk-child-width-1-2@s" uk-grid>
-    <div>
-        <div uk-grid>
-            <div class="uk-width-auto@m">
-                <ul class="uk-list">
-                    <li><RouterLink to="/privacidade" class="uk-button uk-button-text">Politica de Privacidade</RouterLink></li>
+  <div class="uk-container uk-width-1-1 uk-flex uk-margin-medium-top">
+        <div class="uk-margin-medium-right">
+                <ul class="uk-list uk-width-medium uk-background-primary uk-padding-small uk-border-rounded">
+                    <li><RouterLink to="/privacidade" class="uk-button uk-button-text">Política de Privacidade</RouterLink></li>
                     <li><RouterLink to="/cookies" class="uk-button uk-button-text">Política de cookies</RouterLink></li>
                 </ul>
-            </div>
-            <p>ola</p>
+      </div>
+      <div>
+            <article class="uk-article">
+
+    <h1 class="uk-article-title"><a class="uk-link-reset" href="">Política de Privacidade</a></h1>
+
+    <p class="uk-article-meta">Escrito por Parque das Palmeiras em 03 de Novembro 2022.</p>
+
+    <!-- <p class="uk-text-lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p> -->
+
+    <p>A sua privacidade é importante para nós. É política do Parque das Palmeiras respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site Parque das Palmeiras, e outros sites que possuímos e operamos.</p>
+
+    <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
+
+    <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
+
+    <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
+
+    <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
+
+    <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</p>
+
+    <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
+
+    <div class="uk-grid-small uk-child-width-auto" uk-grid>
+        <div>
+            <a class="uk-button uk-button-text" href="#">Read more</a>
+        </div>
+        <div>
+            <a class="uk-button uk-button-text" href="#">5 Comments</a>
         </div>
     </div>
+
+</article>
+</div>          
     
 </div>
 
