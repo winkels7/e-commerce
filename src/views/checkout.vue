ca08353e61d1e3614d0defb638365c7507b2e715
Natália <natalia.cacau23@gmail.com>
2022-11-03 11:52:16 -0300

    nat conserta o bug que ela mesma fez

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -1,15 +1,1 @@
-<template>
-  <div>
-    <h1>CheckoutPage</h1>
-    <div>
-      <h2>Revise seu pedido</h2>
-      <div id="tourEscolhido" v-model="tour"></div>
-      <div id="produtosEvalor" v-model="produtosEvalor"></div>
-      <div id="valorTotal" v-model="valorTotal"></div>
-    </div>
-  </div>
-</template>
 
-<script>
-
-</script>
