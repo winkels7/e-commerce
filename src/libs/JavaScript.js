51abef6b5482b39198245829ceb35f092c4bc5df
Natália <natalia.cacau23@gmail.com>
2022-11-03 10:50:19 -0300

    nat faz coisas

--- src/libs/JavaScript.js
+++ src/libs/JavaScript.js
@@ -1,15 +1,19 @@
-var root = "http://"+document.location.hostname._SERVER['SERVER_PORT']+"/";
 
-$('#botaoPagamento').on('click', function(event) {
-    event.preventDefault();
 
-    console.log(root);
-    $.ajax({
-        url: root+"src/views/controllers/controllerPagamentoDireto.php",
-        type: 'POST',
-        dataType: 'html',
-        success:function(data) {
-            $('body').html(data);
-        }
-    });
-});
+/* var root = 'http://' + document.location.hostname._SERVER['SERVER_PORT'] + '/'
+
+$('#botaoPagamento').on('click', function (event) {
+        event.preventDefault()
+
+        console.log(root)
+        $.ajax({
+                url:
+                        root +
+                        'src/views/controllers/controllerPagamentoDireto.php',
+                type: 'POST',
+                dataType: 'html',
+                success: function (data) {
+                        $('body').html(data)
+                },
+        })
+}) */
