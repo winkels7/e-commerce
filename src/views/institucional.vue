137e80dc14d47929e5327451cf011bdbe049b2c7
iury <iury2002silva@hotmail.com>
2022-11-04 09:40:33 -0300

    terminou-amem-senhor

--- src/views/institucional.vue
+++ src/views/institucional.vue
@@ -3,33 +3,30 @@
         <!-- <div class="uk-margin-medium-right">
                <img src="../assets/Captura de tela de 2022-11-03 08-58-40.png" width="800" alt="">
       </div> -->
-      <h1 class="uk-width-1-2 uk-article-title"> <a class="uk-link-reset">Sobre nós</a></h1>
+      <h1 class="uk-width-1-2 uk-article-title uk-margin-large-right"> <a class="uk-link-reset"> <strong>Institucional</strong></a></h1>
       <div>
             <article class="uk-article">
 
-    <h1 class="uk-article-title"><a class="uk-link-reset">Institucional</a></h1>
+    <h1 class="uk-article-title"><a class="uk-link-reset">O Parque</a></h1>
 
     <p class="uk-article-meta">Escrito por Parque das Palmeiras em 03 de Novembro 2022.</p>
 
     <!-- <p class="uk-text-lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p> -->
 
-    <p>A sua privacidade é importante para nós. É política do Parque das Palmeiras respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site Parque das Palmeiras, e outros sites que possuímos e operamos.</p>
+    <p class="uk-margin-medium-bottom">O Parque das Palmeiras é composto por diversas atrações, desde nossas piscinas termais, piscinas ao ar livre ou aos nossos brinquedos sempre à espera de mais pessoas, tendo ainda como principal atração nossas trilhas cobertas por longas “    estradas” de palmeiras, passando por rios e cachoeiras em seu caminho. Ao entrar no parque, o visitante é recebido pelo “Touro”, monumento que representa o parque ao evidenciá-lo e oferecê-lo aos que entrarem no universo do parque. Seu interior é preenchido pela terra do parque, e seus chifres, revestidos por  semente do cacau, matéria-prima do chocolate, simbolizando alegria. O passeio pelas trilhas é monitorado por guias que contam histórias do parque, como o tobogã dos  Dinossauros, a floresta mágica dos vagalumes, Santuário dos elfos entre outros. A aventura e adrenalina é garantida com os Tobogãs, passeios no dindinho dos furões, ainda contando com o Cinema na caverna, que mantém lampiões presos nas paredes e puffs gigantescos rodeando a tela projetada na parede da caverna. 
+O Parque ainda possui duas grandes lancherias e restaurante para os visitantes fazerem um lanche e almoçarem durante o passeio. No área do tobogã dos dinossauros, o Rex Sorvetes cria todo tipo de dinossauro com os vários sabores de sorvete! O local dispõe também de uma grande loja de souvenirs do Parque e de um fliperama para trazer uma nostalgia aos adultos e um pouco da infância dos pais para as crianças. 
+</p>
 
-    <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
-
-    <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
-
+    
     <div class="uk-margin-medium-right">
-               <img src="../assets/pexels-max-ravier-3436387.jpg" width="800" alt="">
+               <img src="../assets/institucional.jpg" width="600" alt="">
       </div>
-    <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
 
-    <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
+      <h1 class="uk-article-title"><a class="uk-link-reset">Sobre Nós</a></h1>
 
-    <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</p>
+    <p>Inaugurado em 1982, o Parque das Palmeiras levou 4 anos e meio para ser construído. A ideia do parque temático foi de um grupo formado por 5 integrantes, que ao procurar por um terreno para abrigar uma nova loja temática, se encantaram com o som das cachoeiras e o aroma agradável da mata. A partir de então, trabalharam incansavelmente para transformar o sonho do parque temático em realidade.</p>
 
-    <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
-
+   
 
 
 </article>
