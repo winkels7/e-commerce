667d129a89e23ebba02e9d880d571937434bbc7a
nat thunder <62911376+nat-thunder@users.noreply.github.com>
2022-11-03 22:18:49 -0300

    nat continua confusa e tentar vencer o pagseguro

---
+++ src/views/controllers/controllerPagamentoDireto.js
@@ -0,0 +1,16 @@
+// what the frick
+// what the heck i am so confused
+/* PS_TOKEN={{1FC0AC8162704F08975104B4A63F19DB}}
+
+const PagSeguro = require('pagseguro-api')
+const pag = await PagSeguro(true)
+
+$.ajax({
+    url: 'https://ws.sandbox.pagseguro.uol.com.br/v2/checkout',
+    beforeSend: function(xhr) {
+         xhr.setRequestHeader("Authorization", "Bearer 6QXNMEMFHNY4FJ5ELNFMP5KRW52WFXN5")
+    }, success: function(data){
+        alert(data);
+        //process the JSON data etc
+    }
+}) */
