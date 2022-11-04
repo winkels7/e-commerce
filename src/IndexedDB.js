3db186e540b392d6f0f3412b54cd09c33bcdb4e6
Natália <natalia.cacau23@gmail.com>
2022-11-04 10:44:31 -0300

    nat vs. maquiavel epic battle

--- src/IndexedDB.js
+++ src/IndexedDB.js
@@ -1,91 +1,88 @@
-/* 
-function IndexedDB(event) {
-        // o importante é o que importa //
-        alert(`Entrou na função`)
+export default {
+  IndexedDB() {
+    // o importante é o que importa //
+    alert(`Entrou na função`)
+    console.log(`Entrou na função`)
 
-        // teste de compatibilidade
-        const indexedDB =
-                window.indexedDB ||
-                window.mozIndexedDB ||
-                window.webkitIndexedDB ||
-                window.msIndexedDB ||
-                window.shimIndexedDB
+    // teste de compatibilidade
+    const indexedDB =
+      window.indexedDB ||
+      window.mozIndexedDB ||
+      window.webkitIndexedDB ||
+      window.msIndexedDB ||
+      window.shimIndexedDB
 
-        const request = indexedDB.open('parquePalmeiras', 2)
+    const request = indexedDB.open('parquePalmeiras', 2)
 
-        var inteiro = 12
-        var meio = 6
+    var inteiro = 12
+    var meio = 6
 
-        request.onerror = (event) => {
-                alert(`Erro iniciado`)
+    request.onerror = (event) => {
+      alert(`Erro iniciado`)
 
-                console.error('Um erro ocorreu com IndexedDB')
-                console.error(event)
+      console.error('Um erro ocorreu com IndexedDB')
+      console.error(event)
 
-                alert(`Erro encerrado`)
-        }
+      alert(`Erro encerrado`)
+    }
 
-        request.onupgradeneeded = () => {
-                alert(`Upgrade iniciado`)
+    request.onupgradeneeded = () => {
+      alert(`Upgrade iniciado`)
+      console.log(`Upgrade iniciado`)
 
-                const db = request.result
-                const store = db.createObjectStore('ingressos', {
-                        autoIncrement: true,
-                })
-                store.createIndex('ingresso_tipo', ['tipo'], { unique: false })
-                store.createIndex('ingresso_tour', ['tour'], { unique: false })
-                store.createIndex('ingresso_preco', ['preco'], {
+      const db = request.result
+      const store = db.createObjectStore('ingressos', {
+        autoIncrement: true,
+      })
+      /*store.createIndex('ingresso_tipo', ['tipo'], {
                         unique: false,
-                })
+                        })*/
+      store.createIndex('ingresso_tour', ['tour'], {
+        unique: false,
+      })
+      /*store.createIndex('ingresso_preco', ['preco'], {
+                        unique: false,
+                        })*/
+      store.createIndex('ingresso_pago', ['pago'], {
+        unique: false,
+      })
 
-                alert(`Upgrade encerrado`)
-        }
+      alert(`Upgrade encerrado`)
+    }
 
-        request.onsuccess = () => {
-                alert(`Sucesso iniciado`)
+    request.onsuccess = () => {
+      // alert(`Sucesso iniciado`)
+      console.log(`Sucesso iniciado`)
 
-                var tourType = document.getElementById(
-                        'form-stacked-select'
-                ).value
-                var ingInteiro = document.getElementById('inteira').value
-                var ingMeia = document.getElementById('meia').value
-                var ingPCD = document.getElementById('pcd').value
+      var tourType = document.getElementById('form-stacked-select').value
+      var ingInteiro = document.getElementById('inteira').value
+      var ingMeia = document.getElementById('meia').value
+      var ingPCD = document.getElementById('pcd').value
+      var dataTlg = document.getElementById('dataTlg').value
 
-                const db = request.result
-                const transaction = db.transaction('ingressos', 'readwrite')
+      // alert(`${dataTlg}`)
 
-                const store = transaction.objectStore('ingressos')
+      // alert(`inteiro: ${ingInteiro} meia: ${ingMeia} pcd: ${ingPCD}`)
+      console.log(`inteiro: ${ingInteiro} meia: ${ingMeia} pcd: ${ingPCD}`)
 
-                // store.put({ tipo: tipo, tour: tour, preco: preco });
-                if (ingInteiro > 0) {
-                        store.put({
-                                tipo: 'Inteiro',
-                                tour: tourType,
-                                preco: inteiro,
-                                quantidade: ingInteiro,
-                                pago: false,
-                        })
-                }
-                if (ingMeia > 0) {
-                        store.put({
-                                tipo: 'Meia',
-                                tour: tourType,
-                                preco: meio,
-                                quantidade: ingMeia,
-                                pago: false,
-                        })
-                }
-                if (ingPCD > 0) {
-                        store.put({
-                                tipo: 'PCD',
-                                tour: tourType,
-                                preco: meio,
-                                quantidade: ingPCD,
-                                pago: false,
-                        })
-                }
+      const db = request.result
+      const transaction = db.transaction('ingressos', 'readwrite')
 
-                alert(`Sucesso encerrado`)
-        }
-} 
-*/
+      const store = transaction.objectStore('ingressos')
+
+      store.put({
+        inteiro: ingInteiro,
+        meia: ingMeia,
+        pcd: ingPCD,
+        tour: tourType,
+        precoInt: inteiro,
+        precoMei: meio,
+        dataaO: dataTlg,
+        pago: false,
+      })
+
+      // alert(`Sucesso encerrado`)
+      console.log(`Sucesso encerrado`)
+    }
+  },
+}
