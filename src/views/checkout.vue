9b0ad1284762e8674ef7a7984e3cbc56041ca350
Natália <natalia.cacau23@gmail.com>
2022-11-04 10:46:26 -0300

    elena quer jogar d&d

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -1,108 +1,140 @@
 <template>
-   <div
-    class="uk-container-large uk-background-secondary uk-flex uk-flex-around "
-  >
   <div
-    class="uk-container uk-container-medium uk-background-muted uk-width-1-2 uk-margin-large-top uk-border-rounded uk-overflow-auto"
+    class="uk-container-large uk-background-secondary uk-flex uk-flex-around"
   >
-
-  <h3 class="uk-margin-small-top">Produtos</h3>
-  <hr class="uk-divider-small">
-  <div class="uk-overflow-auto">
-    <table class="uk-table uk-table-hover uk-table-middle uk-table-divider">
-        <thead>
+    <div
+      class="uk-container uk-container-medium uk-background-muted uk-width-1-2 uk-margin-large-top uk-border-rounded uk-overflow-auto"
+    >
+      <h3 class="uk-margin-small-top">Produtos</h3>
+      <hr class="uk-divider-small" />
+      <div class="uk-overflow-auto">
+        <table class="uk-table uk-table-hover uk-table-middle uk-table-divider">
+          <thead>
             <tr>
-                <th class="uk-table-shrink">1º</th>
-                <th class="uk-table-shrink"></th>
-                <th class="uk-table-expand">Tipo de produto</th>
-                <th class="uk-width-shrink">Data</th>
-                <th class="uk-width-shrink">Valor</th>
-                <th class="uk-table-shrink uk-text-nowrap">Quantidade</th>
+              <th class="uk-table-shrink">1º</th>
+              <th class="uk-table-shrink"></th>
+              <th class="uk-table-expand">Tipo de produto</th>
+              <th class="uk-width-shrink">Data</th>
+              <th class="uk-width-shrink">Valor</th>
+              <th class="uk-table-shrink uk-text-nowrap">Quantidade</th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             <tr>
-                <td><input class="uk-checkbox" type="checkbox" aria-label="Checkbox"></td>
-                <td><img class="uk-preserve-width uk-border-circle" src="../assets/Icone-ticket-Png-removebg-preview (1).png" width="40" height="40" alt=""></td>
-                <td class="uk-table-link">
-                    <a class="uk-link-reset" href=""> <strong>Histórico</strong><br>
-                      Meia-Entrada</a>
-                </td>
-                <td class="uk-text-truncate">05/11/2022</td>
-                <td class="uk-text-nowrap">R$ 6,00</td>
-                <td class="uk-text-nowrap uk-text-center">1</td>
+              <td>
+                <input
+                  class="uk-checkbox"
+                  type="checkbox"
+                  aria-label="Checkbox"
+                />
+              </td>
+              <td>
+                <img
+                  class="uk-preserve-width uk-border-circle"
+                  src="../assets/Icone-ticket-Png-removebg-preview (1).png"
+                  width="40"
+                  height="40"
+                  alt=""
+                />
+              </td>
+              <td class="uk-table-link">
+                <a class="uk-link-reset" href="">
+                  <strong>Histórico</strong><br />
+                  Meia-Entrada</a
+                >
+              </td>
+              <td class="uk-text-truncate">05/11/2022</td>
+              <td class="uk-text-nowrap">R$ 6,00</td>
+              <td class="uk-text-nowrap uk-text-center">1</td>
             </tr>
-            
-
-           
-        </tbody>
-        
-    </table>
-    <a class="uk-button uk-button-secondary uk-border-rounded">Remover</a>
-
-</div>
-
-
-  </div>
-  <div
-  class="uk-container uk-container-medium uk-background-muted uk-width-1-4 uk-margin-large-top  uk-border-rounded"
-  >
-    <h3 class="uk-margin-small-top">Pedido</h3>
-    <hr class="uk-divider-small">
-    <div class="uk-flex uk-flex-between">
-      <div>
-      <h4> <strong>Total</strong></h4>
+          </tbody>
+        </table>
+        <a class="uk-button uk-button-secondary uk-border-rounded">Remover</a>
+      </div>
     </div>
-    
-    <div>
-      <p>R$ 0,00</p>
-    </div>
-
-    </div>
-
-    <div class="uk-flex uk-flex-between ">
-
-      <div>
-      <h4> <em>Subtotal (1 item)</em></h4>
-    </div>
-    
-    <div>
-      <p class="uk-text-top">R$ 0,00</p>
-    </div>
-
-    </div>
-    
-    <button class="uk-button uk-button-primary uk-margin-small-right uk-border-rounded uk-margin-medium-bottom uk-margin-medium-top" type="button" uk-toggle="target: #modal-example">Finalizar Pedido</button>
-
-    <!--src="https://stc.pagseguro.uol.com.br/public/img/botoes/pagamentos/209x48-pagar-azul-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
-    </form>-->
-    
-    <a class="uk-button uk-button-secondary uk-border-rounded" onclick="window.print()"> <span class="uk-margin-small-right" uk-icon="print"></span>Imprimir</a>
-   
-    <div id="modal-example" uk-modal>
-    <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
-      <button class="uk-modal-close-default" type="button" uk-close></button>
-
-        <h2 class="uk-modal-title">Forma de Pagamento</h2>
-        <div class="uk-margin">
-            <select class="uk-select" aria-label="Select">
-                <option>Cartão</option>
-                <option>Boleto</option>
-                <option>Pix</option>
-              </select>
+    <div
+      class="uk-container uk-container-medium uk-background-muted uk-width-1-4 uk-margin-large-top uk-border-rounded"
+    >
+      <h3 class="uk-margin-small-top">Pedido</h3>
+      <hr class="uk-divider-small" />
+      <div class="uk-flex uk-flex-between">
+        <div>
+          <h4><strong>Total</strong></h4>
         </div>
 
-        
-        <p class="uk-text-right">
-            <button class="uk-button uk-button-default uk-modal-close" type="button">Cancelar</button>
-            <button class="uk-button uk-button-primary" type="button">Concluir</button>
-        </p>
-    </div>
-</div>
+        <div>
+          <p>R$ 0,00</p>
+        </div>
+      </div>
 
+      <div class="uk-flex uk-flex-between">
+        <div>
+          <h4><em>Subtotal (1 item)</em></h4>
+        </div>
+
+        <div>
+          <p class="uk-text-top">R$ 0,00</p>
+        </div>
+      </div>
+
+      <button
+        class="uk-button uk-button-primary uk-margin-small-right uk-border-rounded uk-margin-medium-bottom uk-margin-medium-top"
+        type="button"
+        uk-toggle="target: #modal-example"
+      >
+        Finalizar Pedido
+      </button>
+
+      <a
+        class="uk-button uk-button-secondary uk-border-rounded"
+        onclick="window.print()"
+      >
+        <span class="uk-margin-small-right" uk-icon="print"></span>Imprimir</a
+      >
+
+      <div id="modal-example" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+          <button
+            class="uk-modal-close-default"
+            type="button"
+            uk-close
+          ></button>
+
+          <h2 class="uk-modal-title">Forma de Pagamento</h2>
+          <div class="uk-margin">
+            <select class="uk-select" aria-label="Select">
+              <option>Cartão</option>
+              <option>Boleto</option>
+              <option>Pix</option>
+            </select>
+          </div>
+
+          <p class="uk-text-right">
+            <button
+              class="uk-button uk-button-default uk-modal-close"
+              type="button"
+            >
+              Cancelar
+            </button>
+            <button class="uk-button uk-button-primary" type="button">
+              Concluir
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
-  </div>
-  <div class="uk-container uk-container-small uk-background-secondary">
-    
-  </div>
+  <div class="uk-container uk-container-small uk-background-secondary"></div>
 </template>
+
+<script>
+import obj from '../IndexedDB'
+
+export default {
+  methods: {
+    IndexedDB() {
+      return obj.IndexedDB()
+    },
+  },
+}
+</script>
