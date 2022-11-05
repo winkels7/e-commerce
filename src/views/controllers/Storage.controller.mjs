f252c35146d9f7f10fa7dc74b6d46496125fda55
nat thunder <62911376+nat-thunder@users.noreply.github.com>
2022-11-05 00:55:12 -0300

    nat de volta ao IndexedDB

--- src/views/controllers/Storage.controller.mjs
+++ src/views/controllers/Storage.controller.mjs
@@ -2,9 +2,10 @@ export default class StorageTable {
   constructor() {
     this.db = null
     this.criarBanco()
+    this.criarTable()
   }
 
-  criarBanco(text = 'palmeiras', versao = 2) {
+  criarBanco(text = 'ingressos', versao = 2) {
     const indexedDB =
       window.indexedDB ||
       window.mozIndexedDB ||
@@ -24,8 +25,26 @@ export default class StorageTable {
         autoIncrement: true,
       })
 
-      param.items.forEach((item) => {
+      param.item.forEach((item) => {
         store.createIndex(item.nome, item.chaves, item.params)
+      })
+    }
+  }
+
+  popularTable(param = {}) {
+    this.db.onsuccess = (event) => {
+      console.warn(`Sucesso iniciado`)
+      
+      const database = event.target.result
+      const transaction = database.transaction(param.nome, param.permissao)
+
+      const store = transaction.objectStore(param.action)
+
+      console.warn(`param.nome: ${param.nome} / param.permissao: ${param.permissao} / param.action: ${param.action}`)
+      
+      param.campos.forEach((campos) => {
+        store.put(campos.info)
+        console.warn(`campos.info: ${campos.info}`)
       })
     }
   }
