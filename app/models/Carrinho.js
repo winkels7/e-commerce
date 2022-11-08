1d190de7c75fd4a98d4b6492a025be5f9d261180
João Winkelmann <joaowinkelmann7@gmail.com>
2022-11-07 22:38:57 -0300

    Controlador carrinho

--- app/models/Carrinho.js
+++ app/models/Carrinho.js
@@ -1,19 +1,25 @@
 const database = require('../config/database')
 
-async function queryCarrinho(email) {
+async function buscaCarrinho(usuario) {
   const client = await database()
-  const res = await client.query('SELECT carrinho FROM tb_carrinho WHERE email = $1', [email])
-  return res.rows
+  const sql = `SELECT produto_id, quant FROM tb_carrinho WHERE email = $1;`
+  // console.log(`Email recebido por buscaCarrinho: ${usuario.email}`)
+  const values = [usuario.email]
+  const resposta = await client.query(sql, values)
+  client.release()
+  return resposta.rows
 }
 
-async function addItem(email, item) {
+async function addItem(usuario, item) {
   const client = await database()
-  const sql = `INSERT INTO tb_carrinho (PARAMS) VALUES ($1, $2, $3, $4, $5, $6, $7);`
+  const sql = `INSERT INTO tb_carrinho (email, produto_id, quant) VALUES ($1, $2, $3);`
   const values = [
-    // email,
-    // param,
-    // param2,    
+    usuario.email,
+    item.produto_id,
+    item.quant,
   ]
-  return await client.query(sql, values)
+  const resposta = await client.query(sql, values)
+  client.release()
+  return resposta
 }
-module.exports = { queryCarrinho, addItem }
+module.exports = { buscaCarrinho, addItem }
